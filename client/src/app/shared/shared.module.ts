694e585a8c3ac7b2b3e6c960131cc68b2adea32a
Juni Sulaiman <j.sulaiman@ziggo.nl>
2020-08-26 15:14:43 +0200

    End of section 12

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+
 
 
 @NgModule({
   declarations: [PagingHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
-  exports: [PaginationModule, 
+  exports: [PaginationModule,
             PagingHeaderComponent,
-            PagerComponent]
+            PagerComponent,
+            CarouselModule]
 })
 export class SharedModule { }
