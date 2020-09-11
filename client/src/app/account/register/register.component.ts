8cbcec1b0cad5bdfad90386bcaa39cfe94a1e53e
Juni Sulaiman <j.sulaiman@ziggo.nl>
2020-09-11 14:51:41 +0200

    End of section 17

---
+++ client/src/app/account/register/register.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AsyncValidatorFn } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';
+import { timer, of } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  errors: any;
+  
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
+
+  ngOnInit(): void {    
+    this.createRegisterForm();
+  }
+  
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/shop');
+    }, error => {
+      console.log(error);
+      this.errors = error.errors;
+    });
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      displayName: [null, [Validators.required]],
+      email: [null,
+        [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')],
+        [this.validateEmailNotTaken()]    // asynchronous validation
+      ],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  // asynchronous validator
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return control => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if (!control.value) {
+            return of(null);
+          }
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(res => {
+              return res ? { emailExists: true } : null;
+            })
+          );
+        })
+      );
+    };
+  }
+}
