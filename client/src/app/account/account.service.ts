6dbb704803c8d32d32beb80458a38c4f4f9d5978
Juni Sulaiman <j.sulaiman@ziggo.nl>
2022-04-04 17:05:32 -0400

    End of section 19

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -6,11 +6,13 @@ import { IUser } from '../shared/models/user';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
+import { IAddress } from '../shared/models/address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+ 
   baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<IUser>(1);
   currentUser$ = this.currentUserSource.asObservable();
@@ -68,5 +70,13 @@ export class AccountService {
 
   checkEmailExists(email: string) {
     return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
+  }
+
+  getUserAddress() {
+    return this.http.get<IAddress>(this.baseUrl + 'account/address');
+  }
+  
+  updateUserAddress(address: IAddress) {
+    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
   }
 }
